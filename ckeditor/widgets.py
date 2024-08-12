019f5dbaeba1e22c9bab1029f33b413e79a8e01b
Rudney eduardo souza vieira <50462041+RudneyEduardo@users.noreply.github.com>
2024-08-12 11:56:21 -0300

    Update widgets.py

--- ckeditor/widgets.py
+++ ckeditor/widgets.py
@@ -4,28 +4,12 @@ from django import forms
 from django.conf import settings
 from django.core.exceptions import ImproperlyConfigured
 from django.core.serializers.json import DjangoJSONEncoder
+from django.template.loader import render_to_string
 from django.utils.encoding import force_text
 from django.utils.functional import Promise
 from django.utils.html import conditional_escape
 from django.utils.safestring import mark_safe
 from django.utils.translation import get_language
-
-from js_asset import JS, static
-
-try:
-    # Django >=1.11
-    from django.forms.widgets import get_default_renderer
-except ImportError:
-    # Django <1.11
-    from django.template.loader import render_to_string
-
-    def get_default_renderer():
-        class DummyDjangoRenderer(object):
-            @staticmethod
-            def render(*args, **kwargs):
-                return render_to_string(*args, **kwargs)
-
-        return DummyDjangoRenderer
 
 try:
     # Django >=1.7
@@ -46,7 +30,7 @@ class LazyEncoder(DjangoJSONEncoder):
 json_encode = LazyEncoder().encode
 
 DEFAULT_CONFIG = {
-    'skin': 'moono-lisa',
+    'skin': 'moono',
     'toolbar_Basic': [
         ['Source', '-', 'Bold', 'Italic']
     ],
@@ -70,19 +54,22 @@ class CKEditorWidget(forms.Textarea):
     Widget providing CKEditor for Rich Text Editing.
     Supports direct image uploads and embed.
     """
-
     class Media:
-        js = (
-            JS('ckeditor/ckeditor-init.js', {
-                'id': 'ckeditor-init-script',
-                'data-ckeditor-basepath': getattr(
-                    settings,
-                    'CKEDITOR_BASEPATH',
-                    static('ckeditor/ckeditor/'),
-                ),
-            }),
-            'ckeditor/ckeditor/ckeditor.js',
-        )
+        js = ()
+        jquery_url = getattr(settings, 'CKEDITOR_JQUERY_URL', None)
+        if jquery_url:
+            js += (jquery_url, )
+        try:
+            js += (
+                settings.STATIC_URL + 'ckeditor/ckeditor/ckeditor.js',
+                settings.STATIC_URL + 'ckeditor/ckeditor-init.js',
+            )
+        except AttributeError:
+            raise ImproperlyConfigured("django-ckeditor requires \
+                    CKEDITOR_MEDIA_PREFIX setting. This setting specifies a \
+                    URL prefix to the ckeditor JS and CSS media (not \
+                    uploaded media). Make sure to use a trailing slash: \
+                    CKEDITOR_MEDIA_PREFIX = '/media/ckeditor/'")
 
     def __init__(self, config_name='default', extra_plugins=None, external_plugin_resources=None, *args, **kwargs):
         super(CKEditorWidget, self).__init__(*args, **kwargs)
@@ -111,32 +98,22 @@ class CKEditorWidget(forms.Textarea):
                 raise ImproperlyConfigured('CKEDITOR_CONFIGS setting must be a\
                         dictionary type.')
 
-        extra_plugins = (
-            extra_plugins
-            or self.config.pop("extra_plugins", None)
-            or []
-        )
+        extra_plugins = extra_plugins or []
 
         if extra_plugins:
             self.config['extraPlugins'] = ','.join(extra_plugins)
 
-        self.external_plugin_resources = (
-            external_plugin_resources
-            or self.config.pop("external_plugin_resources", None)
-            or []
-        )
+        self.external_plugin_resources = external_plugin_resources or []
 
-    def render(self, name, value, attrs=None, renderer=None):
-        if renderer is None:
-            renderer = get_default_renderer()
+    def render(self, name, value, attrs=None):
         if value is None:
             value = ''
-        final_attrs = self.build_attrs(self.attrs, attrs, name=name)
+        final_attrs = self.build_attrs(attrs, { 'name': name } )
         self._set_config()
         external_plugin_resources = [[force_text(a), force_text(b), force_text(c)]
                                      for a, b, c in self.external_plugin_resources]
 
-        return mark_safe(renderer.render('ckeditor/widget.html', {
+        return mark_safe(render_to_string('ckeditor/widget.html', {
             'final_attrs': flatatt(final_attrs),
             'value': conditional_escape(force_text(value)),
             'id': final_attrs['id'],
@@ -144,20 +121,6 @@ class CKEditorWidget(forms.Textarea):
             'external_plugin_resources': json_encode(external_plugin_resources)
         }))
 
-    def build_attrs(self, base_attrs, extra_attrs=None, **kwargs):
-        """
-        Helper function for building an attribute dictionary.
-        This is combination of the same method from Django<=1.10 and Django1.11+
-        """
-        attrs = dict(base_attrs, **kwargs)
-        if extra_attrs:
-            attrs.update(extra_attrs)
-        return attrs
-
     def _set_config(self):
-        lang = get_language()
-        if lang == 'zh-hans':
-            lang = 'zh-cn'
-        elif lang == 'zh-hant':
-            lang = 'zh'
-        self.config['language'] = lang
+        if not self.config.get('language'):
+            self.config['language'] = get_language()
